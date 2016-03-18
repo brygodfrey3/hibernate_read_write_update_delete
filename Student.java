70fd1463f85c30d791806185c567bfed0d1b7eb9
Bryant Godfrey <brygodfrey3@gmail.com>
2016-03-18 00:39:34 -0600

    Added files via upload

---
+++ Student.java
@@ -0,0 +1,31 @@
+package com.pretech;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "student", catalog = "hibernateschema")
+public class Student  {
+	@Id
+	@Column(name = "name", unique = true, nullable = false, length = 100)
+	private String name;
+	@Column(name = "standard", unique = true, nullable = false, length = 200)
+	private String standard;
+	
+	public String getStandard() {
+		return standard;
+	}
+	public void setStandard(String standard) {
+		this.standard = standard;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String string) {
+		name = string;
+	}
+	public String toString() {
+		return name;
+	}
+}
